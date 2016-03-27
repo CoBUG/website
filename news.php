c328c726d31e9c24b4a4a4a5f8dfd33742daaa1d
Aaron Bieber <aaron@bolddaemon.com>
2016-03-27 11:25:11 -0600

    add tshirts

--- news.php
+++ news.php
@@ -1,12 +1,19 @@
 <?php
 include('header.php');
 ?>
-    <div class="grid_10">
-        <div class="wfid_temp706829 grid wfid_temp706829">
-            <h2 class="wfid_temp706879 h2">News</h2>
-            <hr size="1" class="wfid_temp706911 hr" />
-            <h3 class="wfid_temp706940 h3">BSDNow interview with Brian Callahan and myself!</h3><time class="wfid_temp706986 time" datetime="2014-05-20 16:00Z">May 20th at 4:00GMT</time>
-            <p class="wfid_temp707020 p">The interview starts at around the 34 minute mark!<br class="wfid_temp707051 br" /><iframe width="560" height="315" src="//www.youtube.com/embed/jYs0ARQo4W8" frameborder="0" allowfullscreen></iframe><br class="wfid_temp707093 br" />Be sure to check out <a href="http://bsdnow.tv" class="wfid_temp707122 link" data-ajax="false">BSDNow</a> for more awesome BSD news!</p>
-<?php
-include('footer.php');
-?>
+<div class="grid_10">
+    <div class="grid">
+	<h2 class="h2">News</h2>
+	<hr size="1" class="hr" />
+	<h3 class="h3">T-Shirts available!</h3>
+	<time class="time" datetime="2016-03-27 11:13Z">March 27th at 11:14GMT</time>
+	<p class="p">Two t-shirts are available, one with a <a href="https://www.amazon.com/dp/B016033DP4">large logo</a> and another with a <a href="https://www.amazon.com/dp/B0179XM7UG">smaller logo</a>. All of the money collected from the shirts goes to funding things like stickers and meetup!</p>
+
+	<h3 class="h3">BSDNow interview with Brian Callahan and myself!</h3>
+	<time class="time" datetime="2014-05-20 16:00Z">May 20th at 4:00GMT</time>
+	<p class="p">The interview starts at around the 34 minute mark!<br class="br" />
+	    <iframe width="560" height="315" src="//www.youtube.com/embed/jYs0ARQo4W8" frameborder="0" allowfullscreen></iframe><br class="br" />
+	    Be sure to check out <a href="http://bsdnow.tv" class="link" data-ajax="false">BSDNow</a> for more awesome BSD news!</p>
+	<?php
+	include('footer.php');
+	?>
