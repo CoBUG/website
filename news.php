aa092f3df1cca8a76f6a700b1bc61930a5150afd
Aaron Bieber <aaron@bolddaemon.com>
2016-05-20 08:12:29 -0600

    news about stickers

--- news.php
+++ news.php
@@ -5,6 +5,13 @@ include('header.php');
     <div class="grid">
 	<h2 class="h2">News</h2>
 	<hr size="1" class="hr" />
+
+	<h3 class="h3">Stickers available!</h3>
+	<time class="time" datetime="2016-05-20 11:13Z">May 20th at 11:12GMT</time>
+	<p class="p">Stickers are available from Sticker Mule!
+	<div style="position: relative; max-width: 320px;"><div style="padding: 30px 0px 0px 0px;display: table;background-color: #e9e9e9;border-top-right-radius: 4px;border-top-left-radius: 4px;text-align: center;position: relative;min-height: 327px;width: 100%;"><div style="display: table-cell;vertical-align: middle;"><img src="https://www.stickermule.com/marketplace/embed_img/12013" style="max-width:100%;"></div></div><div style="padding: 0px 0px 30px 0px;border-bottom-right-radius: 4px;border-bottom-left-radius: 4px;display:table; background-color: #e9e9e9; width:100%;"><div style="text-align: center; display: table-cell;"><a href="https://www.stickermule.com/marketplace/12013-cobug" style="display: inline-block;font-size: 1.2rem;padding: 17px 25px;background-color: #5ba4e6;border-radius: 6px;overflow: hidden;text-align: center;vertical-align: middle;cursor: pointer;border: none;color: #FFF;font-weight: bold;font-family: 'Helvetica Neue',Helvetica, Arial, sans-serif;text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.25);letter-spacing: 0px;line-height: 1.2;-webkit-font-smoothing: antialiased;-webkit-box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.15);-ms-box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.15);-moz-box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.15);-o-box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.15);box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.15);text-decoration: none;">Buy this sticker</a></div></div></div><div style='font-family: "Helvetica Neue", Helvetica, Arial, sans-serif'>Designed by&nbsp;<a href="https://www.stickermule.com/user/1070637306">Aaron Bieber</a> on&nbsp;<a href="https://www.stickermule.com">stickermule.com</a></div>
+	</p>
+
 	<h3 class="h3">T-Shirts available!</h3>
 	<time class="time" datetime="2016-03-27 11:13Z">March 27th at 11:14GMT</time>
 	<p class="p">Two t-shirts are available, one with a <a href="https://www.amazon.com/dp/B016033DP4">large logo</a> and another with a <a href="https://www.amazon.com/dp/B0179XM7UG">smaller logo</a>. All of the money collected from the shirts goes to funding things like stickers and meetup!</p>
