e2b96dfd12620beb5a970e832ea65be474c3bc0c
Aaron Bieber <aaron@bolddaemon.com>
2016-09-27 11:10:32 -0600

    add badge from im observatory

--- xmpp.php
+++ xmpp.php
@@ -6,6 +6,7 @@ include('header.php');
         <h2 class="wfid_temp435541 h2">CoBUG XMPP Service</h2>
         <hr size="1" class="wfid_temp435574 hr" />
         <h3 class="wfid_temp435605 h3">XMPP aka Jabber</h3>
+	<a href='https://xmpp.net/result.php?domain=cobug.org&amp;type=client'><img src='https://xmpp.net/badge.php?domain=cobug.org' alt='xmpp.net score' /></a>
         <p class="wfid_temp435637 p">The xmpp service is availble to "registered" members. These are members that have filled out the form on the <a href="/members" class="wfid_temp435668 link" data-ajax="false">members</a> page.
             <p class="wfid_temp435724 p">Once one the xmpp service, come hang out in the &#39;discussion&#39; chat room!</p>
             <h3 class="wfid_temp435774 h3">Suggested Clients</h3>
