0ef73d025f9d0fec1366971e3b2ddcd9f22b25d0
Aaron Bieber <aaron@bolddaemon.com>
2019-05-01 06:59:07 -0600

    remove g+, add mention to twitter and irc

--- contact.php
+++ contact.php
@@ -8,8 +8,10 @@ include('header.php');
             <h3 class="h3">Email</h3>
             <p class="p">Send questions regarding XMPP, mailing lists and other services to: admin@cobug.org</p>
             <p class="p"></p>
-            <h3 class="h3">Google+</h3>
-            <p class="p">A <a href="https://plus.google.com/u/0/communities/112224531372695972083"class="link"data-ajax="false">Google+ Community</a> exists for general member communication / event planning!</p>
+            <h3 class="h3">IRC</h3>
+            <p class="p">Someone is always on the #cobug IRC channel on Freenode!</p>
+            <h3 class="h3">Twitter</h3>
+            <p class="p"><a href="https://twitter.com/cobsdug">@CoBSDUG</a></p>
 <?php
 include('footer.php');
 ?>
